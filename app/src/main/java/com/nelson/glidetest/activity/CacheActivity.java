f1765a8c9b5c40caaceb72a6d36763c399330436
wanghaoxun <wanghaoxun@guazi.com>
2018-04-13 22:20:05 +0800

    add cache demo

---
+++ app/src/main/java/com/nelson/glidetest/activity/CacheActivity.java
@@ -0,0 +1,59 @@
+package com.nelson.glidetest.activity;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.nelson.glidetest.R;
+import com.nelson.glidetest.databinding.ActivityCacheBinding;
+import com.nelson.glidetest.databinding.ActivityCrossFadeBinding;
+import com.nelson.glidetest.model.ResourceConfig;
+
+/**
+ * Showing memory,disk,and other custom defined cache {@link com.nelson.glidetest.CustomCachingGlideModule}
+ *
+ * Created by Nelson on 2018/4/13.
+ */
+
+public class CacheActivity extends AppCompatActivity {
+
+    private ActivityCacheBinding mBinding;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_cache);
+        setTitle("Cache");
+
+        // memory cache
+        showMemoryCache();
+
+        // disk cache
+        showDiskCache();
+    }
+
+    private void showMemoryCache() {
+        Glide.with(this)
+                .load(ResourceConfig.IMAGE_REMOTE_URLS[0])
+                .placeholder(R.drawable.placeholder)
+                .error(R.drawable.error)
+                .skipMemoryCache(true)
+                .into(mBinding.ivMemory);
+    }
+
+    private void showDiskCache() {
+        Glide.with(this)
+                .load(ResourceConfig.IMAGE_REMOTE_URLS[1])
+                .placeholder(R.drawable.placeholder)
+                .error(R.drawable.error)
+                // no disk cache
+                .diskCacheStrategy(DiskCacheStrategy.NONE)
+                // cache all disk data,contains result and source data
+                //.diskCacheStrategy(DiskCacheStrategy.ALL)
+                // cache source data except result
+                //.diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                .into(mBinding.ivDisk);
+    }
+}
--- app/src/main/java/com/nelson/glidetest/model/ResourceConfig.java
+++ app/src/main/java/com/nelson/glidetest/model/ResourceConfig.java
@@ -8,8 +8,6 @@ import android.os.Environment;
 
 public class ResourceConfig {
 
-    public static final String IMAGE_REMOTE_URL = "http://opkjcw4sd.bkt.clouddn.com/2017.jpg";
-
     public static final String GIF_REMOTE_URL = "http://opkjcw4sd.bkt.clouddn.com/wind_blowing_leaf.gif";
 
     // This is my phone local path,you can replace this with yours.
@@ -18,5 +16,18 @@ public class ResourceConfig {
 
     public static final String VIDEO_LOCAL_PATH =
             Environment.getExternalStorageDirectory() + "/" + "zhangjiajie.mp4";
+
+
+    public static final String[] IMAGE_REMOTE_URLS = {
+            "http://opkjcw4sd.bkt.clouddn.com/anthony-intraversato-257182.jpg",
+            "http://opkjcw4sd.bkt.clouddn.com/photo-1449034446853-66c86144b0ad.jpeg",
+            "http://opkjcw4sd.bkt.clouddn.com/brooke-lark-158017.jpg",
+            "http://opkjcw4sd.bkt.clouddn.com/brooke-lark-254998.jpg",
+            "http://opkjcw4sd.bkt.clouddn.com/christian-joudrey-96208.jpg",
+            "http://opkjcw4sd.bkt.clouddn.com/clement-h-544786.jpg",
+            "http://opkjcw4sd.bkt.clouddn.com/kevin-noble-524437.jpg",
+            "http://opkjcw4sd.bkt.clouddn.com/photo-1483168527879-c66136b56105.jpeg",
+            "http://opkjcw4sd.bkt.clouddn.com/photo-1484893341013-5f1d9c91a96e.jpeg",
+    };
 
 }
